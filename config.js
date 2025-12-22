c537e84d685f16e263e9381ad8d4d3c38d59570c
arthurbrando1-cell <arthur.brando1@aluno.edu.pmpf.rs.gov.br>
2025-12-22 01:17:04 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// Este arquivo armazena suas chaves e credenciais
+const CONFIG = {
+    FIREBASE_API_KEY: "AIzaSyAMHAKVot044psR421B_t-Ht5EmsueVS8A",
+    ADMIN_EMAIL: "contapropredro@gmail.com",
+    ADMIN_PASS: "15423993946" // Senha do Painel
+};
